4c016f76034265730a2c42168da0af921742f56a
Paul <ibohunt1@t6b.com>
2021-01-03 12:47:09 -0600

    format corrections and py3

--- paulstest.py
+++ paulstest.py
@@ -1,6 +1,5 @@
-from time import  sleep
+from time import sleep
 import RPi.GPIO as GPIO
-
 
 DIR = 20
 STEP = 21
@@ -16,7 +15,6 @@ CW = 1
 CCW = 0
 SPR = 1600
 
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
@@ -31,74 +29,70 @@ step_count = SPR - 1200
 delay = .0009
 
 GPIO.output(DIRR, CW)
-GPIO. output(K1, GPIO.LOW)
+GPIO.output(K1, GPIO.LOW)
 GPIO.output(K2, GPIO.HIGH)
-GPIO.output(K3,GPIO.HIGH)
-
+GPIO.output(K3, GPIO.HIGH)
 
 for x in range(step_count):
-	GPIO.output(STEPP, GPIO.HIGH)
-	sleep(delay)
-	GPIO.output(STEPP, GPIO.LOW)
-	sleep(delay)
-
+    GPIO.output(STEPP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEPP, GPIO.LOW)
+    sleep(delay)
 
 sleep(.5)
-print "CW"
+print("CW")
 GPIO.output(DIRR, CCW)
 
-
 for x in range(step_count):
-	GPIO.output(STEPP, GPIO.HIGH)
-	sleep(delay)
-	GPIO.output(STEPP, GPIO.LOW)
-	sleep(delay)
+    GPIO.output(STEPP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEPP, GPIO.LOW)
+    sleep(delay)
 
 sleep(.5)
-print "CCW"
+print("CCW")
 GPIO.output(DIR, CW)
 GPIO.output(K1, GPIO.HIGH)
-GPIO.output(K2,GPIO.LOW)
+GPIO.output(K2, GPIO.LOW)
 
 for x in range(step_count):
-        GPIO.output(STEP, GPIO.HIGH)
-        sleep(delay)
-        GPIO.output(STEP, GPIO.LOW)
-        sleep(delay)
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
 
 sleep(.5)
-print "CW2"
+print("CW2")
 GPIO.output(DIR, CCW)
 
 for x in range(step_count):
-        GPIO.output(STEP, GPIO.HIGH)
-        sleep(delay)
-        GPIO.output(STEP, GPIO.LOW)
-        sleep(delay)
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
 
 sleep(.5)
-print "cCW2"
+print("CCW2")
 GPIO.output(DIRRR, CW)
 GPIO.output(K2, GPIO.HIGH)
 GPIO.output(K3, GPIO.LOW)
 
 for x in range(step_count):
-        GPIO.output(STEPPP, GPIO.HIGH)
-        sleep(delay)
-        GPIO.output(STEPPP, GPIO.LOW)
-        sleep(delay)
+    GPIO.output(STEPPP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEPPP, GPIO.LOW)
+    sleep(delay)
 
 sleep(.5)
-print "CW3"
+print("CW3")
 GPIO.output(DIRRR, CCW)
 
 for x in range(step_count):
-        GPIO.output(STEPPP, GPIO.HIGH)
-        sleep(delay)
-        GPIO.output(STEPPP, GPIO.LOW)
-        sleep(delay)
+    GPIO.output(STEPPP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEPPP, GPIO.LOW)
+    sleep(delay)
 
 GPIO.output(K3, GPIO.HIGH)
-print "CCW3"
+print("CCW3")
 GPIO.cleanup()
-
